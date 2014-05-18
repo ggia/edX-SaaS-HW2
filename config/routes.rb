17f761a84df0738482b6b98c146ca4f2587ac33f
Nikos Papanagiotou <dummy@freemail.gr>
2014-05-18 12:20:47 +0300

    Fixed routes to resolve /movies

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rottenpotatoes::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  #resources :movies
-  root :to => redirect('/movies')
+  resources :movies
+  #root :to => redirect('/movies')
 
   # Sample resource route with options:
   #   resources :products do
