2814512c5969408a84f0bb03380c5f5bd6d50e1d
Nikos Papanagiotou <dummy@freemail.gr>
2014-05-18 12:14:26 +0300

    Added yellow CSS

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rottenpotatoes::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :movies
+  #resources :movies
+  root :to => redirect('/movies')
 
   # Sample resource route with options:
   #   resources :products do
